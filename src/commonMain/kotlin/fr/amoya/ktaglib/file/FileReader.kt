749251e216184c27779c768e1eab7f58d6d0552f
Arnaud <arnaud.moya@gmail.com>
2021-04-30 18:27:29 +0200

    file reading for multiple platforms

---
+++ src/commonMain/kotlin/fr/amoya/ktaglib/file/FileReader.kt
@@ -0,0 +1,17 @@
+package fr.amoya.ktaglib.file
+
+
+/*
+* fr.amoya.ktaglib.file
+* As a part of the Project k-taglib
+* @Author Arnaud Moya : <contact@amoya.fr>
+* Created on 30/04/2021
+*/
+
+
+interface FileReader
+{
+  fun existsAsFile(): Boolean
+  fun isReadable(): Boolean
+  fun readBytes(): ByteArray
+}
---
+++ src/jvmMain/kotlin/fr/amoya/ktaglib/file/FileReaderFactory.kt
@@ -0,0 +1,15 @@
+package fr.amoya.ktaglib.file
+
+
+/*
+* fr.amoya.ktaglib.file
+* As a part of the Project k-taglib
+* @Author Arnaud Moya : <contact@amoya.fr>
+* Created on 30/04/2021
+*/
+
+
+actual object FileReaderFactory
+{
+  actual fun createFileReader(fileName: String): FileReader = JvmFileReader(fileName)
+}
