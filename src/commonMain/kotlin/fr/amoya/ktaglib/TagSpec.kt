af7163f4e7bfd7d80481001f13f6d1ae4e27e958
Arnaud <arnaud.moya@gmail.com>
2021-05-01 00:48:57 +0200

    added some comments

--- src/commonMain/kotlin/fr/amoya/ktaglib/TagSpec.kt
+++ src/commonMain/kotlin/fr/amoya/ktaglib/TagSpec.kt
@@ -8,16 +8,18 @@ package fr.amoya.ktaglib
 * Created on 30/04/2021
 */
 
-
+/**
+ * The "Magic Number" is a Hex representation of the tag inside the file
+ */
 enum class TagSpec(val magicNumber: Long)
 {
   NONE(0),
-  ID3V1(0x54_41_47),
-  ID3V22(0x49_44_33_02),
-  ID3V23(0x49_44_33_03),
-  ID3V24(0x49_44_33_04),
-  FLAC(0x66_4c_61_43),
-  RIFF(0x52_49_46_46),
-  OGG(0x4f_67_67_53),
-  APE(0x41_50_45_54_41_47_45_58)
+  ID3V1(0x54_41_47),              // "TAG"
+  ID3V22(0x49_44_33_02),          // "ID3" + 0x02
+  ID3V23(0x49_44_33_03),          // "ID3" + 0x03
+  ID3V24(0x49_44_33_04),          // "ID3" + 0x04
+  FLAC(0x66_4c_61_43),            // "fLaC"
+  RIFF(0x52_49_46_46),            // "RIFF"
+  OGG(0x4f_67_67_53),             // "OggS"
+  APE(0x41_50_45_54_41_47_45_58)  // "APETAGEX"
 }
--- src/commonMain/kotlin/fr/amoya/ktaglib/parsers/utils/Utils.kt
+++ src/commonMain/kotlin/fr/amoya/ktaglib/parsers/utils/Utils.kt
@@ -14,7 +14,7 @@ import fr.amoya.ktaglib.TagSpec
 object Utils
 {
   fun getTagSpec(rawData: ByteArray): TagSpec =
-    when (rawData.take(4).fold(0L) { aggregate, nextByte -> aggregate shl (8) or nextByte.toLong() })
+    when (buildHexTag(rawData))
     {
       TagSpec.ID3V24.magicNumber -> TagSpec.ID3V24
       TagSpec.ID3V23.magicNumber -> TagSpec.ID3V23
@@ -31,11 +31,21 @@ object Utils
         }
     }
 
-  private fun isId3v1(rawData: ByteArray): Boolean =
-    rawData.takeLast(128).take(3)
-      .fold(0L) { aggregate, nextByte -> aggregate shl (8) or nextByte.toLong() } == TagSpec.ID3V1.magicNumber
+  private fun isAPE(rawData: ByteArray): Boolean = buildHexTag(rawData, 8) == TagSpec.APE.magicNumber
 
-  private fun isAPE(rawData: ByteArray): Boolean =
-    rawData.take(8)
-      .fold(0L) { aggregate, nextByte -> aggregate shl (8) or nextByte.toLong() } == TagSpec.APE.magicNumber
+  /**
+   * ID3v1 is a bit different as the tags are at the end of the file, hence the takeLast call
+   */
+  private fun isId3v1(rawData: ByteArray): Boolean =
+    buildHexTag(rawData.takeLast(128), 3) == TagSpec.ID3V1.magicNumber
+
+  /**
+   * Get a number of bytes from the byte array and aggregates it into one Long
+   * the goal is to rebuild the Magic Number Hex representation
+   */
+  private fun buildHexTag(rawData: ByteArray, numberOfBytes: Int = 4): Long =
+    rawData.take(numberOfBytes).fold(0L) { aggregate, nextByte -> aggregate shl (8) or nextByte.toLong() }
+
+  private fun buildHexTag(rawData: Collection<Byte>, numberOfBytes: Int): Long =
+    rawData.take(numberOfBytes).fold(0L) { aggregate, nextByte -> aggregate shl (8) or nextByte.toLong() }
 }
