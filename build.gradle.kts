1b60701b0fa30ceec4b60fd5722bdf7e7c199f71
Arnaud <arnaud.moya@gmail.com>
2021-04-30 23:01:47 +0200

    added basic tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,62 +1,82 @@
 plugins {
-    kotlin("multiplatform") version "1.4.32"
+  kotlin("multiplatform") version "1.4.32"
+  java
 }
 
 group = "fr.amoya"
 version = "1.0-SNAPSHOT"
 
 repositories {
-    mavenCentral()
+  mavenCentral()
+  jcenter()
+}
+
+dependencies {
+  implementation("org.junit.jupiter:junit-jupiter:5.7.0")
+  implementation("org.junit.jupiter:junit-jupiter-engine:5.7.0")
+}
+
+
+tasks {
+  // Tests
+  withType<Test> {
+    useJUnitPlatform()
+  }
 }
 
 kotlin {
-    targets.all{
-        compilations.all{
-            kotlinOptions{
-                allWarningsAsErrors = true
-            }
-        }
+  targets.all {
+    compilations.all {
+      kotlinOptions {
+        allWarningsAsErrors = true
+      }
     }
+  }
 
-    jvm {
-        withJava()
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        testRuns["test"].executionTask.configure {
-            useJUnit()
-        }
+  jvm {
+    withJava()
+    compilations.all {
+      kotlinOptions.jvmTarget = "1.8"
     }
-    val hostOs = System.getProperty("os.name")
-    val isMingwX64 = hostOs.startsWith("Windows")
-    val nativeTarget = when {
-        hostOs == "Mac OS X" -> macosX64("native")
-        hostOs == "Linux" -> linuxX64("native")
-        isMingwX64 -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    testRuns["test"].executionTask.configure {
+      useJUnitPlatform()
     }
+  }
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  val nativeTarget = when
+  {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux"    -> linuxX64("native")
+    isMingwX64           -> mingwX64("native")
+    else                 -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
 
-    
-    sourceSets {
-        val commonMain by getting {
-            dependencies {
-                implementation(kotlin("stdlib-common"))
-                implementation(kotlin("reflect"))
-            }
-        }
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
-            }
-        }
-        val jvmMain by getting
-        val jvmTest by getting {
-            dependencies {
-                implementation(kotlin("test-junit"))
-            }
-        }
-        val nativeMain by getting
-        val nativeTest by getting
+
+  sourceSets {
+    val commonMain by getting {
+      dependencies {
+        implementation(kotlin("stdlib"))
+        implementation(kotlin("stdlib-common"))
+        implementation(kotlin("reflect"))
+      }
     }
+    val commonTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+        implementation(kotlin("test-common"))
+        implementation(kotlin("test-annotations-common"))
+        implementation(kotlin("test-junit"))
+      }
+    }
+    val jvmMain by getting
+    val jvmTest by getting {
+      dependencies {
+        implementation(kotlin("test"))
+        implementation(kotlin("test-junit"))
+      }
+    }
+    val nativeMain by getting
+    val nativeTest by getting
+  }
 }
---
+++ src/jvmTest/kotlin/fr/amoya/ktaglib/parsers/utils/UtilsTest.kt
@@ -0,0 +1,37 @@
+package fr.amoya.ktaglib.parsers.utils
+
+
+import org.junit.jupiter.api.Assumptions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvFileSource
+import kotlin.io.path.Path
+import kotlin.io.path.exists
+import kotlin.io.path.isReadable
+import kotlin.io.path.isRegularFile
+import kotlin.test.assertEquals
+
+
+/*
+* fr.amoya.ktaglib.parsers.utils
+* As a part of the Project k-taglib
+* @Author Arnaud Moya : <contact@amoya.fr>
+* Created on 30/04/2021
+*/
+
+@kotlin.io.path.ExperimentalPathApi
+internal class UtilsTest
+{
+  @ParameterizedTest(name = "{1}")
+  @DisplayName("Should return right file type")
+  @CsvFileSource(resources = ["/expected_results.csv"], numLinesToSkip = 1)
+  fun shouldReturnRightFileType(filename: String, expectedType: String)
+  {
+    val fPath = Path("src", "commonTest", "resources", filename)
+
+    Assumptions.assumeTrue(fPath.exists() && fPath.isRegularFile() && fPath.isReadable())
+    assertEquals(expectedType, "${Utils.getTagSpec(fPath.toFile().readBytes())}")
+  }
+
+
+}
