cde66a233ac167f5af5d1dabfc9f1916b4bc96bd
Arnaud Moya <dev@amoya.fr>
2021-06-01 18:34:09 +0200

    removed depreciation as I might use this method to analyze files in the future

--- src/commonMain/kotlin/fr/amoya/ktaglib/platformdependent/file/FileReader.kt
+++ src/commonMain/kotlin/fr/amoya/ktaglib/platformdependent/file/FileReader.kt
@@ -15,14 +15,7 @@ interface FileReader
   val cursor: Long
 
   fun load(filename: String)
-
   fun readNBytes(size: String): ByteArray
-
-  @Deprecated(
-    message = "This function is not optimized as it reads the whole file at once",
-    replaceWith = ReplaceWith("readNBytes"),
-    DeprecationLevel.WARNING
-  )
   fun readEntireFile(): Sequence<Byte>
   fun close()
 }
